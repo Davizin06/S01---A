0571db01184d2637ac9a3b3aea409809e3185fcf
Davi Marcondes <152312917+Davizin06@users.noreply.github.com>
2025-11-06 23:11:37 -0300

    Refactor Castelo class and add adicionarDivisao method
    Removed comments and added a method to add divisions.

--- relatorio_11/Castelo.java
+++ relatorio_11/Castelo.java
@@ -2,10 +2,8 @@ import java.util.ArrayList;
 
 public class Castelo {
 
-    // Atributo público, conforme diagrama [cite: 104]
     public String nome;
     
-    // Atributo privado para a lista de divisões [cite: 106]
     private ArrayList<Divisao> divisoes;
 
     public Castelo(String nome, ArrayList<Divisao> divisoes) {
@@ -13,19 +11,13 @@ public class Castelo {
         this.divisoes = divisoes;
     }
     
-    /**
-     * Construtor alternativo para criar um castelo
-     * e inicializar sua lista de divisões vazia.
-     */
     public Castelo(String nome) {
         this.nome = nome;
         // Importante: Inicializa a lista
         this.divisoes = new ArrayList<>();
     }
 
-    /**
-     * Requisito 4: Implementa o método adicionarDivisao [cite: 92]
-     */
+
     public void adicionarDivisao(Divisao divisao) {
         this.divisoes.add(divisao);
         System.out.println("A divisão '" + divisao.nome + "' foi adicionada ao " + this.nome + ".");
